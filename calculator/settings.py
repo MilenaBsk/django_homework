e67b9ef768d483261490eef47aeb6117876bd822
MilenaBsk <milenabeltowska@gmail.com>
2023-05-18 23:06:13 +0200

    fixed calculator function

--- calculator/settings.py
+++ calculator/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # local apps
     'my_calculator.apps.MyCalculatorConfig',
     'monday.apps.MondayConfig',
+    # 'businesscard.apps.BussinescardC'
 
     # 'add.apps.AddConfig'
 ]
--- my_calculator/views.py
+++ my_calculator/views.py
@@ -9,38 +9,48 @@ def calculator(request):
 
 
 def add(request, value1, value2):
+
+    suma = int(value1) + int(value2)
     return render(
-         request,
-         'add.html',
-         context={
-             "value1": value1,
-             "value2": value2
-         })
+     request,
+     'add.html',
+     context={
+         "value1": value1,
+         "value2": value2,
+         "suma": suma
+
+     })
 
 def substract(request, value1, value2):
+    subs_result = int(value1) - int(value2)
     return render(
          request,
          'substract.html',
          context={
              "value1": value1,
-             "value2": value2
+             "value2": value2,
+             "subs_result": subs_result
          })
 
 def multiply(request, value1, value2):
+    multi_result =int(value1) * int(value2)
     return render(
          request,
          'multiply.html',
          context={
              "value1": value1,
-             "value2": value2
+             "value2": value2,
+             "multi_result": multi_result
          })
 
 def divide(request, value1, value2):
+    div_result = int(value1) / int(value2)
     return render(
          request,
          'divide.html',
          context={
              "value1": value1,
-             "value2": value2
+             "value2": value2,
+             "div_result": div_result
          })
 
