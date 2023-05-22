ee80bbbdb9c5879a749d7a54460259126eec6868
MilenaBsk <milenabeltowska@gmail.com>
2023-05-22 10:15:05 +0200

    random int

--- draw/views.py
+++ draw/views.py
@@ -1,9 +1,21 @@
+import random
+
 from django.shortcuts import render
 
 # Create your views here.
 
 def toto_lotek(request):
+    random_num = random.randint(1, 49)
+
+
     return render(
         request,
-        'toto_lotek.html'
+        'toto_lotek.html',
+        context={
+            'random_num': random_num
+
+    }
     )
+
+
+    return render(request, 'random_number.html', {'random_num': random_num})
